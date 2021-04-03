b3333f726f25485df81add27e62f1b6145f4e995
Christopher-Robin Fey <chiny1@web.de>
2021-04-03 20:29:59 +0200

    added fully working API JERSEY impl

---
+++ api/RestAPI/src/main/java/com/fi9e/rest/resources/MyResource.java
@@ -0,0 +1,31 @@
+
+package com.fi9e.rest.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+/** Example resource class hosted at the URI path "/hi"
+ */
+@Path("/hi")
+public class MyResource {
+    
+    /** Method processing HTTP GET requests, producing "text/plain" MIME media
+     * type.
+     * @return String that will be send back as a response of type "text/plain".
+     */
+    @GET 
+    @Produces("text/plain")
+    public String getIt() {
+        return "Hi there!";
+    }
+    
+    @GET
+    @Path("/{name}")
+    public Response getItHello(@PathParam("name") String name) {
+    	String result = "Hello "+ name;
+    	return Response.status(200).entity(result).build();
+    }
+}
