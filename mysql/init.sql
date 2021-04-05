51fc22c8dee8b2a2381a10f7af2cac4cd0a809b2
vaighir <vaighir@gmail.com>
2021-04-05 15:29:43 +0200

    Rework user table in init.sql

--- mysql/init.sql
+++ mysql/init.sql
@@ -6,8 +6,10 @@ use test_db;
 CREATE TABLE user (
     id INT AUTO_INCREMENT,
     name VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255),
     PRIMARY KEY (id)
 );
 
-INSERT INTO user (name) values ("John Smith");
-INSERT INTO user (name) values ("Jane Shepard");
+INSERT INTO user (name) values ("John Smith", "john.smith@mail.com", "passwtr");
+INSERT INTO user (name) values ("Jane Shepard", "jane.shepard@normandy.gov", "pwt");
