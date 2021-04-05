ecb119016c438e9d306c855042fb3ce057c43cd9
vaighir <vaighir@gmail.com>
2021-04-05 16:21:10 +0200

    Update dao test files

--- api/RestAPI/src/main/java/com/fi9e/rest/dao/HibernateTest.java
+++ api/RestAPI/src/main/java/com/fi9e/rest/dao/HibernateTest.java
@@ -4,66 +4,67 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import com.fi9e.rest.entity.User;
 
 public class HibernateTest {
 
-public static void main(String[] args) {
-		
+	public static void main(String[] args) {
+
 		// create session factory
-		
-		SessionFactory factory = new Configuration()
-				.configure("hibernate.cfg.xml")
-				.addAnnotatedClass(User.class)
+
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(User.class)
 				.buildSessionFactory();
-		
-		// create session 
+
+		// create session
 
 		Session session = factory.getCurrentSession();
-		
+
 		try {
 			// use the session object to save Java object
-			
-			
+
 			// create a real message object
 			System.out.println("Creating a new RealMessage object");
-			User tempUser = new User("Tom Riddle", "tom.riddle@test.com", "oasswrod"); 
-			
+			User tempUser = new User("Jake Fiddle", "jake.fiddle@test.com", "oawrewrod");
+
 			// start a transaction
 			session.beginTransaction();
-			
+
 			// save the User object
 			System.out.println("Saving the User into the database...");
 			session.save(tempUser);
-			
+
 			// commit transaction
 			session.getTransaction().commit();
-			
+
 			System.out.println("Done!");
-			
-		    // Code for UPDATE:
-			
+
+			// Code for UPDATE:
+
 			// find out the id (primary key) of the RealMessage
 			System.out.println("Saved user. Generated id: " + tempUser.getId());
-			
+
 			// not get a new session and start transaction
 			session = factory.getCurrentSession();
 			session.beginTransaction();
-			
-			//retrieve RealMessage based on the id: primary key
+
+			// retrieve RealMessage based on the id: primary key
 			System.out.println("\nGetting user with id: " + tempUser.getId());
-			
+
 			User myRealUser = session.get(User.class, tempUser.getId());
-			
+
 			System.out.println("Get complete: " + myRealUser);
-			
+
 			// commit the transaction
 			session.getTransaction().commit();
 		}
-		
+
 		finally {
+			if (session != null && session.isOpen()) {
+				session.close();
+			}
 			factory.close();
 		}
-		
+
 	}
 
 }
