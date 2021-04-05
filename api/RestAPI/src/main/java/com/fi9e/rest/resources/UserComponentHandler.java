2696bf314580ee2572fcbc29a95bfd44b5e61a2b
vaighir <vaighir@gmail.com>
2021-04-05 16:20:38 +0200

    Implement first version of user dao in user handler

--- api/RestAPI/src/main/java/com/fi9e/rest/resources/UserComponentHandler.java
+++ api/RestAPI/src/main/java/com/fi9e/rest/resources/UserComponentHandler.java
@@ -3,17 +3,22 @@ package com.fi9e.rest.resources;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
+
+import com.fi9e.rest.entity.User;
+import com.fi9e.rest.dao.UserDao;
 
 @Path("/user")
 public class UserComponentHandler {
+	UserDao userDao = new UserDao();
 	
 	@GET
 	@Path("/{id}")
-	public Response getUserById() {
-		String user = "Peter Knerz";
+	public Response getUserById(@PathParam("id") int id) {
+		User user = userDao.getUserById(id);
 		
-		return Response.status(200).entity(user).build();
+		return Response.status(200).entity(user.toString()).build();
 	}
 	
 	
