6fd54b668b37060af366561073460ea22315027e
vaighir <vaighir@gmail.com>
2021-04-05 14:30:45 +0200

    Change init.sql to create tables for v1 api

--- mysql/init.sql
+++ mysql/init.sql
@@ -3,21 +3,11 @@ DROP DATABASE IF EXISTS test_db;
 CREATE DATABASE test_db;
 use test_db;
 
-CREATE TABLE real_message (
+CREATE TABLE user (
     id INT AUTO_INCREMENT,
-    message LONGTEXT,
-    author VARCHAR(255),
-    server VARCHAR(255),
-    channel VARCHAR(255),
+    name VARCHAR(255),
     PRIMARY KEY (id)
 );
 
-CREATE TABLE dictionary (
-    id INT AUTO_INCREMENT,
-    author VARCHAR(255),
-    dictionary JSON,
-    PRIMARY KEY (id)
-);
-
-INSERT INTO real_message (author, message, channel, server) values ("bassaf", "asfsadf", "awrge", "rwgfv");
-INSERT INTO real_message (author, message, channel, server) values ("user2", "hello", "say-hi", "darknet");
+INSERT INTO user (name) values ("John Smith");
+INSERT INTO user (name) values ("Jane Shepard");
