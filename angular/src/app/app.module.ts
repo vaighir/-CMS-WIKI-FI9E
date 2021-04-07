72e10c79cbc9b01a20155163e8823159c70df181
Sukyeong Hong <s.hong@orangehive.de>
2021-04-07 21:15:28 +0200

    added nav, main and header component in angular

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './main/header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
+    MainComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
