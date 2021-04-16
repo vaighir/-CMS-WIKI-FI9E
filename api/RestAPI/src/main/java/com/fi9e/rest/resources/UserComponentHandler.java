1915fa8b378da3871ac8206a1c4e63485f61635e
Sukyeong Hong <s.hong@orangehive.de>
2021-04-16 21:12:56 +0200

    deleted build.xml

--- api/RestAPI/src/main/java/com/fi9e/rest/resources/UserComponentHandler.java
+++ api/RestAPI/src/main/java/com/fi9e/rest/resources/UserComponentHandler.java
@@ -4,15 +4,27 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
+import com.google.gson.Gson;
+
+
+
 @Path("/user")
 public class UserComponentHandler {
 	
 	@GET
 	@Path("/{id}")
 	public Response getUserById() {
+
+		User user = new User(1234, "Peter", "p1123@google.com");
+
+		Gson gson = new Gson();
+		String jsonString = gson.toJson(user);
+
+		 System.out.println(jsonString);
+		return Response.status(200).entity(jsonString).build();;
+		/*
 		String user = "Peter Knerz";
-		
-		return Response.status(200).entity(user).build();
+		return Response.status(200).entity(user).build();*/
 	}
-	
+
 }
