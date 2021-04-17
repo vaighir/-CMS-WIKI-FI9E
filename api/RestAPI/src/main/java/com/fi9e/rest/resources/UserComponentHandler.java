8f1a104ac3da2ff6a4c81ae1067b9cc10b4a4e5b
vaighir <vaighir@gmail.com>
2021-04-17 02:04:55 +0200

    Solve merge conflict

--- api/RestAPI/src/main/java/com/fi9e/rest/resources/UserComponentHandler.java
+++ api/RestAPI/src/main/java/com/fi9e/rest/resources/UserComponentHandler.java
@@ -6,27 +6,33 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
+import com.google.gson.Gson;
+
 import com.fi9e.rest.entity.User;
 import com.fi9e.rest.dao.UserDao;
 
 @Path("/user")
 public class UserComponentHandler {
 	UserDao userDao = new UserDao();
-	
+
 	@GET
 	@Path("/{id}")
 	public Response getUserById(@PathParam("id") int id) {
 		User user = userDao.getUserById(id);
-		
-		return Response.status(200).entity(user.toString()).build();
+
+		Gson gson = new Gson();
+		String jsonString = gson.toJson(user);
+
+		System.out.println(jsonString);
+		return Response.status(200).entity(jsonString).build();
+
 	}
-	
-	
+
 	@DELETE
 	@Path("/{2}")
 	public Response deleteUserById() {
 		String user = "Peter Knerz";
-		
+
 		return Response.status(200).entity(user).build();
 	}
 }
