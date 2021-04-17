0ff0135b3ce8632411d4e48fb45ec5a48433d436
vaighir <vaighir@gmail.com>
2021-04-17 10:19:16 +0200

    Implement user dao test class

---
+++ api/RestAPI/src/main/java/com/fi9e/rest/dao/UserDaoTest.java
@@ -0,0 +1,21 @@
+package com.fi9e.rest.dao;
+
+import com.fi9e.rest.entity.User;
+
+public class UserDaoTest {
+	public static void main(String[] args) {
+		UserDao ud = new UserDao();
+
+		ud.createUser("Jackaf Sparrow", "john.smith@test.com", "password");
+		ud.createUser("Katewr Johnson", "kate.johnson@test.com", "waefsd");
+
+		User u1 = ud.getUserById(15);
+
+		System.out.println(u1);
+
+		u1.setEmail("new_email@tet.com");
+
+		ud.updateUser(u1);
+	}
+
+}
