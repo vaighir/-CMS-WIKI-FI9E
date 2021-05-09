b2cc873074171e19a74409cac4f902d8987a6a14
vaighir <vaighir@gmail.com>
2021-05-09 09:04:16 +0200

    Fixed docker bugs

--- api/RestAPI/src/main/java/com/fi9e/rest/entity/Article.java
+++ api/RestAPI/src/main/java/com/fi9e/rest/entity/Article.java
@@ -63,6 +63,22 @@ public class Article {
 		this.id = id;
 	} */
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	
 	public String getSlug() {
 		return slug;
 	}
