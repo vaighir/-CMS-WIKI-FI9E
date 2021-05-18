bb0589ba1c36c0f3aed7078fc58cd319294c1a92
Sukyeong Hong <s.hong@orangehive.de>
2021-05-18 20:16:33 +0200

    bugfix - all articles link

--- angular/src/app/main/articles/components/articles/articles.component.ts
+++ angular/src/app/main/articles/components/articles/articles.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NavMenuService } from 'src/app/nav-menu/services/nav-menu.service';
 import { ArticleModel } from '../../model/article-model.Model';
 import { ArticleService } from '../../services/article-service.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-articles',
@@ -9,25 +10,33 @@ import { ArticleService } from '../../services/article-service.service';
   styleUrls: ['./articles.component.scss']
 })
 export class ArticlesComponent implements OnInit {
-  allArticleList: any;
   ArticleListByCategory?: any;
   articleList?: ArticleModel[];
   categoryId?: any;
   allArticles: boolean;
+  checkIfAllArticles: string[];
 
   constructor(
     private articleService: ArticleService,
-    private navMenuService: NavMenuService
+    private navMenuService: NavMenuService,
+    private router: Router
+
   ) {
-    this.allArticleList = null;
     this.allArticles = true;
+    this.checkIfAllArticles = this.router.routerState.snapshot.url.split('/');
+
   }
 
   ngOnInit(): void {
 
+    if(this.checkIfAllArticles[1] === 'articles'){
+      this.articleService.articleList()
+        .subscribe(items => this.articleList = items);
+    }
     this.navMenuService.currentCategoryId.subscribe(
       id => this.getArticlesByCategoryId(id)
     )
+
   }
 
   getArticlesByCategoryId(id: number): void {
@@ -37,9 +46,6 @@ export class ArticlesComponent implements OnInit {
       this.allArticles = false;
       this.categoryId = id;
 
-    } else {
-      this.articleService.articleList()
-        .subscribe(items => this.articleList = items);
     }
   }
 
