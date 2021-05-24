f58198c74e5cc64c69d23e51c126a0761b83c6f7
Christopher-Robin Fey <chiny1@web.de>
2021-05-24 16:55:43 +0200

    added error interceptor http

--- angular/src/app/http-interceptor/http-interceptor.ts
+++ angular/src/app/http-interceptor/http-interceptor.ts
@@ -1,5 +1,6 @@
-import { HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 
 export class AddHeaderInterceptor implements HttpInterceptor {
@@ -22,3 +23,24 @@ export class AddHeaderInterceptor implements HttpInterceptor {
   }
 }
 
+export class HttpErrorInterceptor implements HttpInterceptor {
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request)
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          let errorMsg = '';
+          if (error.error instanceof ErrorEvent) {
+            console.log('this is client side error');
+            errorMsg = `Error: ${error.error.message}`;
+          }
+          else {
+            console.log('this is server side error');
+            errorMsg = `Error Code: ${error.status},  Message: ${error.message}`;
+          }
+          console.log(errorMsg);
+          return throwError(errorMsg);
+        })
+      )
+  }
+  
+}
