049b166563aa1dd7c15522e57a3d65b1cf9c62fa
Wiktor Sierpinski <vaighir@gmail.com>
2021-06-05 06:09:41 +0000

    Update deployment scripts

--- deployment/build_and_run.sh
+++ deployment/build_and_run.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-docker volume rm cms-wiki-fi9e_tomcat
+# Stop the application if running
+docker-compose down
+#docker volume rm cms-wiki-fi9e_tomcat
 
+# Build the Java app
 mvn clean install -f ./api/RestAPI/pom.xml
 
+# Build the Angular app
 cd angular
 npm install
 npm update
 ng build --configuration production
 cd ..
 
-docker-compose down
+# Start the app in detached mode
 docker-compose up -d
